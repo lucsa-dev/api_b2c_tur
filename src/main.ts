8eacf3fb015ce22c1fe03b28b98947b2347b2f1c
Lucas Almeida <lucsa.dev@gmail.com>
2024-08-07 07:55:58 -0300

    feat: config sweggar

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
+import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
+import { NotFoundInterceptor } from './common/errors/interceptors/notFound.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bolerplate NestJS')
+    .setDescription('Bolerplate NestJS API description')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        description: 'Bearer Authentication',
+        type: 'http',
+      },
+      'access_token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ConflictInterceptor());
+  app.useGlobalInterceptors(new DatabaseInterceptor());
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
+  app.useGlobalInterceptors(new NotFoundInterceptor());
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
